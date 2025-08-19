98405454d633ccf2816401bff20a13e85b37b3c2
John Sweeney <email@sarno.dev>
2025-08-19 22:06:40 +0100

    12 changes by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,2 +1,15 @@
 export default defineAppConfig({
+  header: {
+    title: "PoE Community Help"
+  },
+
+  seo: {
+    title: "PoE Community Help",
+    description: "The best resource for Path of Exile players encountering technical issues!"
+  },
+
+  github: {
+    url: "",
+    branch: ""
+  }
 })
