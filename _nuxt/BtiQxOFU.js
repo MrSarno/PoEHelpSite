51314772376ca5125154b83a257e3c977256b3fd
MrSarno <email@sarno.dev>
2025-08-19 21:10:39 +0000

    Deploying to gh-pages from @ MrSarno/PoEHelpSite@98405454d633ccf2816401bff20a13e85b37b3c2 🚀

---
+++ _nuxt/BtiQxOFU.js
@@ -0,0 +1,1 @@
+import{r}from"./B0q7i9mk.js";import e from"./BTRLuOXb.js";import{z as t,H as a,i as c,w as n,W as p}from"./Bon6Olxp.js";const l={__name:"ProseTip",setup(i){const o=t();return(s,f)=>(c(),a(e,{color:"success",icon:p(o).ui.icons.tip},{default:n(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
