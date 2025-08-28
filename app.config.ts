935d37b9bd1c9e16862379b8ee05d8a0aced5f4c
John Sweeney <email@sarno.dev>
2025-08-28 21:39:31 +0100

    feat: Test footer changes

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,14 @@ export default defineAppConfig({
     title: "PoE Community Help"
   },
 
+  footer: {
+    credits: {
+      icon: '',
+      text: `© ${new Date().getFullYear()} John Sweeney // Sarno#0943   |   This website is neither affiliated with, nor endorsed by, Grinding Gear Games`,
+      href: '/'
+    },
+  },
+  
   seo: {
     title: "PoE Community Help",
     description: "The best resource for Path of Exile players encountering technical issues!"
