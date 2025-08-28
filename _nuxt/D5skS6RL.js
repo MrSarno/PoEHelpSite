040c8fd8c3b2ab61ecd7a0891305be417fe88d66
MrSarno <email@sarno.dev>
2025-08-28 14:55:08 +0000

    Deploying to gh-pages from @ MrSarno/PoEHelpSite@aaebf0a77f43a05e72096f3b098749b1aa644dec 🚀

---
+++ _nuxt/D5skS6RL.js
@@ -0,0 +1,1 @@
+import{r as e}from"./DAiqMX-h.js";import n from"./jX_E740S.js";import{z as s,H as t,i as a,w as i,W as c}from"./CcMXNfXR.js";const l={__name:"ProseNote",setup(p){const o=s();return(r,f)=>(a(),t(n,{color:"info",icon:c(o).ui.icons.info},{default:i(()=>[e(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
