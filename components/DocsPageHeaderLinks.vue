18d793797b4926b7ae51cd4bb6123e756c8bca10
MrSarno <email@sarno.dev>
2025-08-28 21:05:07 +0100

    feat: Remove "Copy page" dialogue from the top-right

---
+++ components/DocsPageHeaderLinks.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "DocsPageHeaderLinks.vue"
+})
+</script>
+
+<template>
+&nbsp;
+</template>
+
+<style scoped>
+
+</style>
