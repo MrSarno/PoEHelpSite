f635384b5689a87f1aa29836c5887d24da11e057
John Sweeney <email@sarno.dev>
2025-08-29 21:01:38 +0100

    1 change by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -1,18 +1,7 @@
 export default defineAppConfig({
   header: {
     title: "PoE Community Help"
-  }, footer: {
-        credits: {
-            icon: '', // optional; remove or change icon
-            text: `© ${new Date().getFullYear()} PoE Community Help`,
-            href: '/' // or your GitHub/Legal page/etc.
-        },
-        // Optional: links shown in the footer center
-        textLinks: [
-            { text: 'Contact', href: '/contact' },
-            { text: 'Privacy', href: '/privacy' }
-        ]
-    }
+  },
 
   footer: {
     credits: {
@@ -21,7 +10,7 @@ export default defineAppConfig({
       href: '/'
     },
   },
-  
+
   seo: {
     title: "PoE Community Help",
     description: "The best resource for Path of Exile players encountering technical issues!"
@@ -32,5 +21,12 @@ export default defineAppConfig({
     branch: ""
   },
 
-  socials: {}
+  socials: {},
+
+  ui: {
+    colors: {
+      primary: "cyan",
+      neutral: "slate"
+    }
+  }
 })
