1b5207ffc4f3f740210a1959d727ab2d9a60ae72
MrSarno <email@sarno.dev>
2025-09-02 17:55:45 +0000

    Deploying to gh-pages from @ MrSarno/PoEHelpSite@78ed9cbb73aaa4d5ac76f16c86f65103f6769a67 🚀

---
+++ _nuxt/A8ii8Jlw.js
@@ -0,0 +1,1 @@
+import{r as a}from"./BQAkBdvb.js";import t from"./BIeX0WMr.js";import{z as e,H as n,i as s,w as c,W as i}from"./B6riMW6A.js";const l={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(t,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{l as default};
