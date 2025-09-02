240ef94bfad829f8e9f84034717fcd7f14ea83da
MrSarno <email@sarno.dev>
2025-09-02 15:55:06 +0100

    chore: Fix

--- app.config.ts
+++ app.config.ts
@@ -1,22 +1,14 @@
 export default defineAppConfig({
+  github: false,
+
   header: {
     title: "PoE Community Help"
-  },
-
-  footer: {
-    credits: {
-      icon: '',
-      text: `© ${new Date().getFullYear()} John Sweeney // Sarno#0943   |   This website is neither affiliated with, nor endorsed by, Grinding Gear Games`,
-      href: '/'
-    },
   },
 
   seo: {
     title: "PoE Community Help",
     description: "The best resource for Path of Exile players encountering technical issues!"
   },
-
-  github: false,
 
   socials: {},
 
