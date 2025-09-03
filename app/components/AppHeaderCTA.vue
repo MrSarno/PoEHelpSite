feff62995b97360ba53353e2b3d2e81db0b8e68b
MrSarno <email@sarno.dev>
2025-09-03 18:15:37 +0100

    style: Have links 'pop' out to visually clarify where each link begins & ends

--- app/components/AppHeaderCTA.vue
+++ app/components/AppHeaderCTA.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-sm">
-    Consider supporting on <a href="https://patreon.com/PoeCommunityHelp">Patreon</a> or <a href="https://buymeacoffee.com/poecommunityhelp">BuyMeACoffee</a>
+    Consider supporting on <b><a href="https://patreon.com/PoeCommunityHelp">Patreon</a></b> or <b><a href="https://buymeacoffee.com/poecommunityhelp">BuyMeACoffee</a></b>
   </div>
 </template>
 <script setup lang="ts">
