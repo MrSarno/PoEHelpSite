c8647313de33d53c5070ad32b2ce9ee786f0d3c7
MrSarno <email@sarno.dev>
2025-09-03 18:03:07 +0100

    feat: Adding CTA to the header

---
+++ app/components/AppHeaderCTA.vue
@@ -0,0 +1,5 @@
+<template>
+  Consider supporting on [Patreon](https://patreon.com/PoeCommunityHelp) or [BuyMeACoffee](https://buymeacoffee.com/poecommunityhelp)
+</template>
+<script setup lang="ts">
+</script>
