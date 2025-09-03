c98e799ee6dd23113ba0f2db299ae2a9a901d405
MrSarno <email@sarno.dev>
2025-09-03 17:15:21 +0100

    feat: Custom footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <UFooter>
+    <template #left>
+      <div class="text-sm text-muted">
+        Copyright © John Sweeney, 2025
+      </div>
+    </template>
+    <template #right>
+      <div class="text-sm"; style="text-align: center">
+        This website is neither affiliated with, nor endorsed by, Grinding Gear Games
+      </div>
+      <UColorModeButton />
+    </template>
+  </UFooter>
+</template>
