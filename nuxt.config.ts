093dc5526a70a3f22b267931b92c68a708f3f09e
MrSarno <email@sarno.dev>
2025-09-04 23:59:05 +0100

    ChatGPT throwing more dice

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,17 @@ export default defineNuxtConfig({
     extends: ['docus'],
     ssr: true,
     app: { baseURL: '/' },
+
+    //
+    content: {
+        documentDriven: true,
+        prerender: true
+    },
+
     nitro: {
         prerender: {
             crawlLinks: true,
-            routes: ['/']
+            routes: ['/' ]
         }
     }
 })
